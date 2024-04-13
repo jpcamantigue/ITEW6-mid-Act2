18bec534b753cc26c782849f8ff2f68063996634
jpcamantigue <133960309+jpcamantigue@users.noreply.github.com>
2024-04-13 12:25:03 +0800

    Create productlist.vue

---
+++ src/components/productlist.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+      <Product v-for="product in products" :key="product.id" :product="product" />
+    </div>
+  </template>
+  
+  <script>
+  import Product from './Product.vue';
+  
+  export default {
+    components: {
+      Product,
+    },
+    data() {
+      return {
+        products: [
+          { id: 1, name: 'Product 1', description: 'Description for Product 1', price: 10 },
+          { id: 2, name: 'Product 2', description: 'Description for Product 2', price: 20 },
+          // More products...
+        ],
+      };
+    },
+  };
+  </script>
